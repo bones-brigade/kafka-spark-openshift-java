97e36985a7c00149ff311f51d027232f37b14556
Michael McCune <msm@redhat.com>
2018-03-07 15:56:00 -0500

    add basic kafka example
    Much of this is borrowed from the upstream word count example in the
    spark repository.

--- src/main/java/org/bonesbrigade/skeletons/kafkasparkopenshift/App.java
+++ src/main/java/org/bonesbrigade/skeletons/kafkasparkopenshift/App.java
@@ -1,7 +1,51 @@
 package org.bonesbrigade.skeletons.kafkasparkopenshift;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.api.java.*;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.kafka010.ConsumerStrategies;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("alive");
+    public static void main(String[] args) throws Exception {
+        String brokers = System.getenv("KAFKA_BROKERS");
+        String topic = System.getenv("KAFKA_TOPIC");
+
+        if (brokers == null) {
+            System.out.println("KAFKA_BROKERS must be defined.");
+            System.exit(1);
+        }
+        if (topic == null) {
+            System.out.println("KAFKA_TOPIC must be defined.");
+            System.exit(1);
+        }
+
+        SparkConf conf = new SparkConf().setAppName("KafkaSparkOpenShiftApp");
+        JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(3));
+
+        Set<String> topicSet = new HashSet<String>();
+        topicSet.add(topic);
+        Map<String, Object> kafkaParams = new HashMap<String, Object>();
+        kafkaParams.put("metadata.brokers.list", brokers);
+
+        JavaInputDStream<ConsumerRecord<Object, Object>> messages = KafkaUtils.createDirectStream(
+            jssc,
+            LocationStrategies.PreferConsistent(),
+            ConsumerStrategies.Subscribe(topicSet, kafkaParams));
+
+        JavaDStream<String> lines = messages.map(l -> l.value().toString());
+        lines.print();
+
+        jssc.start();
+        System.out.println("waiting for input");
+        jssc.awaitTermination();
     }
 }
